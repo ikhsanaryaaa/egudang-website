b7ff8a999cf2f69f2aad184bfc94a1da9aeee1fe
ikhsanaryaaaa <ikhsanaryaaa@gmail.com>
2026-05-16 03:22:22 +0700

    install roles and permissions seeder (closes #22)
    - Create RoleAndPermissionSeeder
    
    - Define Manager, Kepala Gudang, and Operator Gudang roles
    
    - Define basic permissions for inventory management
    
    - Map permissions to roles
    
    - Register seeder in DatabaseSeeder

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        $permissions = [
+            'view users',
+            'manage users',
+            'view products',
+            'create products',
+            'edit products',
+            'delete products',
+            'view categories',
+            'create categories',
+            'edit categories',
+            'delete categories',
+            'view stock movements',
+            'create stock movements',
+            'edit stock movements',
+            'delete stock movements',
+            'view reports',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // create roles and assign existing permissions
+
+        // Manager role
+        $roleManager = Role::create(['name' => 'Manager']);
+        $roleManager->givePermissionTo(Permission::all());
+
+        // Kepala Gudang role
+        $roleKepalaGudang = Role::create(['name' => 'Kepala Gudang']);
+        $roleKepalaGudang->givePermissionTo([
+            'view products',
+            'create products',
+            'edit products',
+            'delete products',
+            'view categories',
+            'create categories',
+            'edit categories',
+            'delete categories',
+            'view stock movements',
+            'create stock movements',
+            'edit stock movements',
+            'delete stock movements',
+            'view reports',
+        ]);
+
+        // Operator Gudang role
+        $roleOperatorGudang = Role::create(['name' => 'Operator Gudang']);
+        $roleOperatorGudang->givePermissionTo([
+            'view products',
+            'view categories',
+            'view stock movements',
+            'create stock movements',
+        ]);
+    }
+}
